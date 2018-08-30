9e84a20a36704e53a2e27c2019164337abc9dff8
Pavan <pavan.powani@practo.com>
2018-08-30 11:24:27 +0530

    Allow single selection in each section

--- RadiusAssignment/Radius/RadiusViewController.swift
+++ RadiusAssignment/Radius/RadiusViewController.swift
@@ -61,22 +61,37 @@ class RadiusViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: RadiusTableViewCell = tableView.dequeueReusableCell(withIdentifier: "RadiusCell", for: indexPath) as! RadiusTableViewCell // ?? UITableViewCell(style: .default, reuseIdentifier: "RadiusCell")
-        if indexPath.section == 0 {
+        
+        switch indexPath.section {
+        case 0:
             print(indexPath.section)
             cell.radiusCellLabel?.text = radiusViewModel?.radius?.facilities[indexPath.section].options[indexPath.row].name
             if let imageName = radiusViewModel?.radius?.facilities[indexPath.section].options[indexPath.row].icon {
                 cell.radiusCellImage?.image = UIImage(named: imageName)
             }
-        } else if indexPath.section == 1 {
+        case 1:
             print(indexPath.section)
             cell.radiusCellLabel.text = radiusViewModel?.radius?.facilities[indexPath.section].options[indexPath.row].name
             cell.radiusCellImage?.image = nil
-        } else {
+        case 2:
             print(indexPath.section)
             cell.radiusCellLabel.text = radiusViewModel?.radius?.facilities[indexPath.section].options[indexPath.row].name
             cell.radiusCellImage?.image = nil
+        default:
+            break
         }
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if let selectedIndexPaths = tableView.indexPathsForSelectedRows {
+            for selectedIndexPath in selectedIndexPaths {
+                if selectedIndexPath.section == indexPath.section {
+                    tableView.deselectRow(at: selectedIndexPath, animated: false)
+                }
+            }
+        }
+        return indexPath
     }
 
 
